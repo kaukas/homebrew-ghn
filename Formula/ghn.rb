5873f6f2fbdcc8c3b7461923543eac69d84d430f
Linas Juškevičius <linas@prodigito.lt>
2020-04-29 01:22:37 +0300

    Fixed installation of 0.1

--- Formula/ghn.rb
+++ Formula/ghn.rb
@@ -1,11 +1,12 @@
 class Ghn < Formula
   desc "GitHub notifications in your terminal"
   homepage "https://github.com/kaukas/ghn"
-  url "https://github.com/kaukas/ghn/releases/download/untagged-b63fe3a6ecc4c1a6022a/ghn-0.1.tar.gz"
+  url "https://github.com/kaukas/ghn/releases/download/0.1/ghn-0.1.tar.gz"
+  sha256 "9b0a044c27f03a8687f15758115fdd4e00bf05e3f1da935c87145baa4d4e1fbc"
   head "https://github.com/kaukas/ghn.git"
 
   def install
-    bin.install bin
-    libexec.install lib
+    bin.install ["bin/ghn"]
+    libexec.install Dir["libexec/*"]
   end
 end
